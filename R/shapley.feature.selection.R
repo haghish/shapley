79f9d223eabe6df6d8d95cb6c02749f9a67ec948
E. F. Haghish <haghish.e.f@gmail.com>
2025-03-18 22:02:58 +0100

    0.4.31

--- R/shapley.feature.selection.R
+++ R/shapley.feature.selection.R
@@ -4,20 +4,13 @@
 #'              for plotting SHAP contributions for each row, or summary of absolute
 #'              SHAP contributions for each feature.
 #' @param shapley shapley object
-#' @param method character, specifying the method used for identifying the most
-#'               important features according to their weighted SHAP values.
-#'               The default selection method is "lowerCI", which includes
-#'               features whose lower weighted confidence interval exceeds the
-#'               predefined 'cutoff' value (default is relative SHAP of 1%).
-#'               Alternatively, the "mean" option can be specified, indicating
-#'               any feature with normalized weighted mean SHAP contribution above
-#'               the specified 'cutoff' should be selected. Another
-#'               alternative options is "shapratio", a method that filters
-#'               for features where the proportion of their relative weighted SHAP
-#'               value exceeds the 'cutoff'. This approach calculates the relative
-#'               contribution of each feature's weighted SHAP value against the
-#'               aggregate of all features, with those surpassing the 'cutoff'
-#'               being selected as top feature.
+#' @param method Character. The column name in \code{summaryShaps} used
+#'                           for feature selection. Default is \code{"mean"}, which
+#'                           selects important features which have weighted mean shap
+#'                           ratio (WMSHAP) higher than the specified cutoff. Other
+#'                           alternative is "lowerCI", which selects features which
+#'                           their lower bound of confidence interval is higher than
+#'                           the cutoff.
 #' @param cutoff numeric, specifying the cutoff for the method used for selecting
 #'               the top features. the default is zero, which means that all
 #'               features with the "method" criteria above zero will be selected.
@@ -30,7 +23,7 @@
 #' @export
 
 shapley.feature.selection <- function(shapley,
-                                      method = "lowerCI",
+                                      method = "mean",
                                       cutoff=0.0,
                                       top_n_features=NULL,
                                       features = NULL) {
@@ -55,17 +48,13 @@ shapley.feature.selection <- function(shapley,
       shapley$summaryShaps <- shapley$summaryShaps[shapley$summaryShaps$mean > cutoff, ]
       shapley$contributionPlot$data <- DATA[DATA$feature %in% features, ]
 
-    } else if (method == "shapratio") {
-      shapley$summaryShaps <- shapley$summaryShaps[shapley$summaryShaps$shapratio > cutoff, ]
-      shapley$contributionPlot$data <- DATA[DATA$feature %in% features, ]
-
     } else if (method == "lowerCI") {
       if (length(shapley[["ids"]]) == 1) stop("shapley object includes a single model and lowerCI cannot be used")
       shapley$summaryShaps <- shapley$summaryShaps[shapley$summaryShaps$lowerCI > cutoff, ]
       shapley$contributionPlot$data <- DATA[DATA$feature %in% features, ]
 
     } else {
-      stop("method must be one of 'mean', 'shapratio', or 'ci'")
+      stop("method must be one of 'mean' or 'lowerCI'")
     }
   }
   else (stop("at least 1 model must be included in the shapley object"))
@@ -75,12 +64,10 @@ shapley.feature.selection <- function(shapley,
   index <- order(- shapley$summaryShaps$mean)
   features <- features[index]
   mean <- shapley$summaryShaps$mean[index]
-  shapratio <- shapley$summaryShaps$shapratio[index]
 
   return(list(shapley = shapley,
               features = features,
-              mean = mean,
-              shapratio = shapratio))
+              mean = mean))
 
 }
 
