690b963caf26af0eeea62f6454ea8900e803f799
E. F. Haghish <haghish.e.f@gmail.com>
2026-02-11 15:14:54 +0100

    0.6.3.9

--- R/h2o.get_ids.R
+++ R/h2o.get_ids.R
@@ -1,8 +1,9 @@
-#' @title h2o.get_ids
-#' @description extracts the model IDs from H2O AutoML object or H2O grid
+#' @title Extract model IDs from H2O AutoML or Grid objects
+#' @description Extracts model IDs from a \code{"H2OAutoML"} object (via the leaderboard)
+#'              or from a \code{"H2OGrid"} object.
 #' @importFrom h2o h2o.get_leaderboard
-#' @param automl a h2o \code{"AutoML"} grid object
-#' @return a character vector of trained models' names (IDs)
+#' @param h2oboard An object inheriting from \code{"H2OAutoML"} or \code{"H2OGrid"}.
+#' @return A character vector of trained model IDs.
 #' @author E. F. Haghish
 #' @examples
 #'
@@ -16,11 +17,11 @@
 #' aml <- h2o.automl(y = y, training_frame = prostate, max_runtime_secs = 30)
 #'
 #' # get the model IDs
-#' ids <- h2o.ids(aml)
+#' ids <- h2o.get_ids(aml)
 #' }
 #' @export
 
-h2o.get_ids <- function(automl) {
-  if (inherits(automl,"H2OAutoML")) return(as.vector(h2o.get_leaderboard(automl)[,1]))
-  else if (inherits(automl,"H2OGrid")) return(as.vector(unlist(automl@model_ids)))
+h2o.get_ids <- function(h2oboard) {
+  if (inherits(h2oboard,"H2OAutoML")) return(as.vector(h2o.get_leaderboard(h2oboard)[,1]))
+  else if (inherits(h2oboard,"H2OGrid")) return(as.vector(unlist(h2oboard@model_ids)))
 }
